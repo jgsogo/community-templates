cd4cfeae637a193e1b631fc0154c8e4725fb50c7
jgsogo <jgsogo@gmail.com>
2020-07-14 19:44:33 +0200

    list directories

---
+++ .script/main.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import argparse
+import logging
+from typing import Union, Text, Iterator, Tuple
+
+PATH = Union[bytes, Text]
+me = os.path.abspath(os.path.dirname(__file__))
+log = logging.getLogger('templates')
+
+
+def configure_log():
+    log.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+def get_templates(template_dir: PATH) -> Iterator[Tuple[str, PATH]]:
+    for tpl in os.listdir(template_dir):
+        tpl_path: PATH = os.path.join(template_dir, tpl)
+        if os.path.isdir(tpl_path):
+            yield tpl, os.path.join(template_dir, tpl)
+
+def main(working_dir: PATH, args: argparse.Namespace):
+    log.debug(f"Generate output for templates '{args.templates}'")
+    log.debug(f" - output directory: '{working_dir}")
+    templates_dir = os.path.abspath(os.path.join(me, '..', args.templates))
+    for name, path in get_templates(templates_dir):
+        log.info(f"{name}: {path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create docs for templates')
+    #parser.add_argument('--working-dir', type=str, help='working directory')
+    parser.add_argument('--templates', type=str, help='templates to generate')
+    args = parser.parse_args()
+
+    working_dir = os.path.abspath(os.path.join(me, '..', '_working_dir'))
+    if os.path.exists(working_dir):
+        shutil.rmtree(working_dir)
+    os.mkdir(working_dir)
+
+    configure_log()
+    main(working_dir, args)
