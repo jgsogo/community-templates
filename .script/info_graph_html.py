e86aad6c3526e3078bbbe93b2dabe34527d1566c
jgsogo <jgsogo@gmail.com>
2020-07-15 00:37:46 +0200

    use file:// prefix for local file

--- .script/info_graph_html.py
+++ .script/info_graph_html.py
@@ -62,9 +62,10 @@ class InfoGraphHTML:
         stdout, stderr = process.communicate()
 
         # Take the picture
+        output_file = os.path.abspath(output_file)
         picture_file = name + '.png'
         #process = subprocess.Popen(['wkhtmltoimage', '--debug-javascript', '--javascript-delay', '5000', output_file, picture_file],
-        process = subprocess.Popen(['firefox', '-headless', '--screenshot', picture_file, output_file],
+        process = subprocess.Popen(['firefox', '-headless', '--screenshot', picture_file, f"file://{output_file}"],
                      stdout=subprocess.PIPE, 
                      stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
